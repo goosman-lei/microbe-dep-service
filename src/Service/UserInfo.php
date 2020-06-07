7d005dd2146c2499d68d5326818527e9d193aadd
goosman.lei <goosman.lei@gmail.com>
2020-06-07 11:17:39 +0800

    init commit

---
+++ src/Service/UserInfo.php
@@ -0,0 +1,11 @@
+<?php
+namespace User;
+class UserInfo extends \Microbe\Service {
+    public function getInfo($name) {
+        return [
+            'id'   => 1,
+            'name' => $name,
+            'age'  => 19,
+        ]
+    }
+}
