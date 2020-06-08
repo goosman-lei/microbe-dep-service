3341f025ba10f5fbcfb9e3d1d9624e121ae404a5
goosman.lei <goosman.lei@gmail.com>
2020-06-09 00:24:41 +0800

    update service

--- src/Service/UserInfo.php
+++ src/Service/UserInfo.php
@@ -3,9 +3,10 @@ namespace MicrobeDep\Service;
 class UserInfo extends \Microbe\Service {
     public function getInfo($name) {
         return [
-            'id'   => 1,
-            'name' => $name,
-            'age'  => 19,
+            'id'    => 1,
+            'name'  => $name,
+            'sayhi' => 'Hello, ' . $name,
+            'age'   => 20,
         ];
     }
 }
